9e90d4a78094c689486957f538a752b04f866fa3
Balthier1337 <raptors.ftw@gmail.com>
2016-07-12 13:08:44 +0100

    2.1
    Restarting services now uses an array and a function
    Added support for the -WhatIf switch (Still logs as normal)

--- ArchiveScript.ps1
+++ ArchiveScript.ps1
@@ -4,24 +4,30 @@
 # Created: 2014-01-21
 # Modified: 2014-10-30
 # Script: Recursive Folder Backup
-# Version: 2.0
+# Version: 2.1
 # Description: A quick PowerShell script for the backup of an entire directory, to another directory
-#
+# Change log:
+# 
 
 # 
 # DEFINE ALL NECESSARY VARIABLES
 # 
 
+[CmdletBinding()]
+Param(
+[switch]$WhatIf
+)
+
 # Current Date
 $Date = Get-Date
 # Timespan used for ShowOldFiles
 $Timespan = new-timespan -days 30
-# Define service(s) to be stopped
+# Define service(s) to be cycled
 # NOTE: Must be the "Service Name", not the "Display Name" of the service
-$Service_1 = ""
-# Original file location
+$Services = "",""
+# Original file location, without trailing \
 $OrigPath = ""
-# Backup folder location
+# Backup folder location, without trailing \
 $BackupPath = ""
 # Define today's backup folder
 $BackupDir = "{0}-{1:d2}-{2:d2}" -f $date.year, $date.month, $date.day
@@ -41,9 +47,9 @@ Stop-Transcript | out-null
 # Action to take on errors - "Continue": Continue on error, display errors
 $ErrorActionPreference = "Continue"
 # Create log folder
-New-Item $BackupErrorLogDir -type directory -force
+New-Item $BackupErrorLogDir -Type Directory -Force
 # Begin logging to log file
-Start-Transcript -path "$BackupErrorLogDir\$BackupErrorLogFile" -append
+Start-Transcript -Path "$BackupErrorLogDir\$BackupErrorLogFile" -Append
 
 # 
 # DEFINE ALL NECESSARY FUNCTIONS
@@ -62,16 +68,49 @@ function RemoveOldFiles($Path, $Timespan)
 		$LastWrite = (get-item $Loc).LastWriteTime
         # Check if the file is older than the timespan
 		if (((get-date) - $LastWrite) -gt $Timespan) {
-             # Delete the file, if it is older
-             # NOTE: Add -WhatIf to the command, to perform a "Test Run"
-			 Remove-Item -path $Loc -Force -Recurse -Verbose -WhatIf
+			# Delete the file, if it is older
+			Remove-Item -path $Loc -Force -Recurse -Verbose -WhatIf:$WhatIf
 		}
-        # Uncomment this statement while using -WhatIf for better debugging
-        # Runs if the file is newer than the timespan
-        #else {
+        # Only runs when -WhatIf is specified
+        elseif ($WhatIf) {
             # prints "Newer : " followed by the filename
-		#	echo "Newer : $File"
-		#}
+		    echo "Newer : $File"
+		}
+	}
+}
+
+# Starts/Stops all services in $Services
+function ToggleServices($ServicesArray, $Toggle) {
+    # Build the correct toggle command
+    $ToggleService = "$Toggle`-Service"
+    # Check if we are stopping the service
+    if ($Toggle -eq "Stop") { 
+        # Set the ForceFlag to make sure our service is stopped
+        $ForceFlag = "True" 
+    }
+    # Loop through items in $ServicesArray
+    Foreach ($Service in $ServicesArray) {
+        # Get the details of the currently loaded service
+        $ServiceStatus = Get-Service $Service
+        # Check if the ForceFlag variable is set
+        if ($ForceFlag) {
+            # Force the service to stop
+            & $ToggleService $Service -Force -Verbose -WhatIf:$WhatIf
+            # Check to make sure we don't wait for the service to start while using -WhatIf
+            if ($WhatIf -eq "True") {
+                # Wait until the service has been stopped
+                $ServiceStatus.WaitForStatus('Stopped')
+            }
+        }
+        else {
+            # Start the service
+            & $ToggleService $Service -Verbose -WhatIf:$WhatIf
+            # Check to make sure we don't wait for the service to start while using -WhatIf
+            if ($WhatIf -eq "True") {
+                # Wait until the service has been started
+                $ServiceStatus.WaitForStatus('Running')
+            }
+        }
 	}
 }
 
@@ -79,19 +118,19 @@ function RemoveOldFiles($Path, $Timespan)
 # BEGIN BACKUP PROCESS
 # 
 
-# Stop the associated service
-Stop-Service $Service_1 -WhatIf
+# Stop the associated services
+ToggleServices $Services "Stop"
 # Create today's backup folder
-New-Item $BackupPath\$BackupDir -type directory -Force -WhatIf
+New-Item $BackupPath\$BackupDir -type directory -Force -Verbose -WhatIf:$WhatIf
 # Copy all files from the original location
-Copy-Item $OrigPath\* $BackupPath\$BackupDir -Force -WhatIf
+Copy-Item $OrigPath\* $BackupPath\$BackupDir -Force -Verbose -WhatIf:$WhatIf
 
 # 
 # BEGIN CLEAN UP PROCESS
 # 
 
 # Start the associated service
-Start-Service $Service_1 -WhatIf
+ToggleServices $Services "Start"
 # Checks backup files 
 RemoveOldFiles $BackupPath $Timespan
 # Stops logging process
